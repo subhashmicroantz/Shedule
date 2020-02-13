14c5738282b96932831f9fc7071e24e1a0538481
soundarya1627 <you@example.com>
2020-02-13 06:32:46 -0800

    c sharp programs

---
+++ CSharpSamples/MethodCls.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpSamples
+{
+    public class MethodCls
+    {
+        public int i;
+        public int x;
+        public int y;
+        public int z;
+
+        public string str;
+
+        public void DefaultMethod()
+        {
+            Console.WriteLine("Subash name");
+        }
+
+        internal void MethodIntx(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MethodAdd()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MethodInty(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MethodP(string name)
+        {
+            Console.WriteLine("My Name is " + name);
+        }
+
+
+        public void MethodA(string name2)
+        {
+            Console.WriteLine(name2);
+        }
+
+        public void MethodIntx(int x)
+        {
+            Console.WriteLine(x);
+
+        }
+        public void MethodInty(int y)
+        {
+            Console.WriteLine(y);
+        }
+
+        public void MethodAdd(int z)
+        {
+            Console.WriteLine(z = x + y);
+        }
+
+    
+
+    }
+}
---
+++ CSharpSamples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MethodCls obj = new MethodCls();
+
+            obj.DefaultMethod();
+
+            obj.MethodP("Vijay");
+
+            obj.MethodA("viji");
+
+            obj.MethodIntx("10");
+            obj.MethodInty("5");
+            obj.MethodAdd();
+
+        }
+
+        }
+    }
